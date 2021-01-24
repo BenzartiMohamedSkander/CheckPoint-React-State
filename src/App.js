636c04f4725eb2e6528923fcbaa5589ef1a23b9b
unknown <benzarti.skander@gmail.com>
2021-01-24 21:37:28 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import Profile from './profile/Profile'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import React, { Component } from 'react'
+
+ class App extends Component {
+
+  state =  {Person : {fullName :"Mohamed",bio:"Student", imgSrc:"/profil2.png",profession:"Developer" },
+  show:false
 }
+//  let show =false;
+
+
+ handleClick=()=>this.setState({show:!this.state.show})
+
+
+  render() {
+    return (
+      
+      <div>
+<br/><br/><br/>
+
+  <div className="d-grid gap-2 col-6 mx-auto">
+  <button className="btn btn-primary" type="button"  onClick={this.handleClick}>
+  {this.state.show ? "Hide Profile" : "Show Profile"} </button>
+</div>
+
+{this.state.show  && <Profile fullName={this.state.Person.fullName}  profession={this.state.Person.profession} bio={this.state.Person.bio} imgSrc={this.state.Person.imgSrc}  /> }
+
+
+      
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
 
 export default App;
